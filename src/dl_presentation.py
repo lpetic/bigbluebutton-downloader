978f5da652c404fa8df3a0652dd122b407aadbef
Lucian Petic <lucian.petic14@gmail.com>
2021-05-04 19:25:23 +0200

    using wget library instead of system call

--- src/dl_presentation.py
+++ src/dl_presentation.py
@@ -5,6 +5,9 @@ import shutil
 import time
 import cairosvg
 import validators
+import wget
+import urllib
+
 from PyPDF2 import PdfFileMerger
 
 def dl_presentation(url: str):
@@ -15,25 +18,34 @@ def dl_presentation(url: str):
     # Processing the url
     arr = url.split('/')
     if arr[-2] == "svg":
+        arr = arr[:-2]
         url = '/'.join(arr)
     else:
         print("Incorrect patern.")
         return -1
 
     tmp = time.time()
-    out = '_output_' + str(tmp)
+    out = str(tmp)
 
     os.mkdir(out)
+    os.mkdir(out + '/svg')
+    os.mkdir(out + '/pdf')
     
     # Merge .pdf files to one
     merger = PdfFileMerger()
-
+    
     i = 1
-    while err := os.system('wget {}/svg/{} -O {}/{}.svg -q -nv'.format(url, i, out, i)) == 0:
+    while True:
+        try:
+            wget.download('{}/svg/{}'.format(url, i), out='{}/{}/'.format(out, 'svg'))
+        except urllib.error.HTTPError:
+            break
+
+        print(' ' + str(i))
         # Path to svg and pdf
-        form = '{}/{}.{}'
-        svgFile = form.format(out, i, 'svg')
-        pdfFile = form.format(out, i, 'pdf')
+        form = '{}/{}/{}'
+        svgFile = form.format(out, 'svg', i)
+        pdfFile = form.format(out, 'pdf', i)
         # Convert svg to pdf
         cairosvg.svg2pdf(url = svgFile, write_to = pdfFile)
         # Merge to final pdf
@@ -42,7 +54,7 @@ def dl_presentation(url: str):
 
     if i != 1:
         merger.write(('{}.pdf').format(tmp))
-        print("Final .pdf was created.")
+        print("Final .pdf was created with {} pages.".format(i - 1))
     else:
         print("Download fail.")
 
